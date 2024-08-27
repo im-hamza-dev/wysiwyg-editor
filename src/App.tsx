4c8e2db4f3e63cad82c74a6ead69074ef6a322ba
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-08-27 22:39:28 +0500

    feature: popup toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.scss";
 import Editor from "./components/quillEditor/quillEditor.component";
 import Quill from "quill";
@@ -7,11 +7,47 @@ import TurndownService from "turndown";
 const Delta = Quill.import("delta");
 
 const App = () => {
-  const [range, setRange] = useState();
+  const [range, setRange] = useState<any>();
   const [lastChange, setLastChange] = useState<any>();
+  const [showToolbar, setShowToolbar] = useState(false);
+  const [toolbarPosition, setToolbarPosition] = useState<any>({});
+
   let highlights = ["entrepreneur", "biological age"];
   // Use a ref to access the quill instance directly
   const quillRef: any = useRef();
+  const toolbarRef: any = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (toolbarRef.current && !toolbarRef.current?.contains(event.target)) {
+        setShowToolbar(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    const openToolbar = () => {
+      if (!quillRef?.current) {
+        return;
+      }
+      const quillEditor = quillRef?.current;
+
+      if (range?.length > 0) {
+        const bounds = quillEditor.getBounds(range.index);
+        setToolbarPosition({
+          top: bounds.top + 15,
+          left: bounds.left - 15,
+        });
+        setShowToolbar(true);
+      }
+    };
+    openToolbar();
+  }, [range]);
 
   const searchKeywords = () => {
     if (quillRef.current) {
@@ -41,12 +77,35 @@ const App = () => {
     <div className="app-wrapper">
       <h1> Demo for WYSIWYG Editor</h1>
       <div className="cards-wrapper">
-        <div>
+        <div className="editor-parent">
+          <div
+            ref={toolbarRef}
+            className={`quill-popup-toolbar ${
+              showToolbar ? "showToolbar" : "hideToolbar"
+            }`}
+            style={{
+              top: `${toolbarPosition.top}px`,
+              left: `${toolbarPosition.left}px`,
+            }}
+          >
+            <button onClick={() => quillRef.current.format("bold", true)}>
+              Bold
+            </button>
+            <button onClick={() => quillRef.current.format("italic", true)}>
+              Italic
+            </button>
+            <button onClick={() => quillRef.current.format("underline", true)}>
+              Underline
+            </button>
+            {/* Add more formatting options as needed */}
+          </div>
+
           <Editor
             ref={quillRef}
             onSelectionChange={setRange}
             onTextChange={setLastChange}
           />
+
           <button className="search-btn" onClick={searchKeywords}>
             Search
           </button>
